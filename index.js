240e4c1b87fdbc34420d14efb5a8fe99d747f0bf
Gil <gilbertjvirgo@gmail.com>
2021-11-04 14:57:52 +0000

    Made it directly runnable

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const { exec } = require("child_process");
 const http = require("http");
 dotenv.config();
 
-module.exports = () => {
-	var handler = createHandler({
-		path: "/webhook",
-		secret: process.env.GIT_SECRET, // Use the .env file
-	});
+// module.exports = () => {
+var handler = createHandler({
+	path: "/webhook",
+	secret: process.env.GIT_SECRET, // Use the .env file
+});
 
-	http.createServer((req, res) => {
-		handler(req, res, res.statusCode(404).end("Not found"));
-	}).listen(7777, () => console.log("Starting webhook handler..."));
+http.createServer((req, res) => {
+	handler(req, res, res.statusCode(404).end("Not found"));
+}).listen(7777, () => console.log("Starting webhook handler..."));
 
-	handler.on("error", console.error);
+handler.on("error", console.error);
 
-	handler.on("push", () => {
-		console.log("Recieved a push event!");
+handler.on("push", () => {
+	console.log("Recieved a push event!");
 
-		exec(`bash ./deploy.sh >> ./local`);
-	});
-};
+	exec(`bash ./deploy.sh >> ./local`);
+});
+// };
